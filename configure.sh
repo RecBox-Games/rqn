de7c6595af29a4b59a75c5a59403a8606fb3836e
Safwaan Menk <41082275+safwaan-menk@users.noreply.github.com>
2024-02-03 21:05:13 -0800

    release:safwaan:140d3 | fruit merge updates

--- configure.sh
+++ configure.sh
@@ -70,45 +70,9 @@ dpkg -l | grep -qw $QRENCODE
 # Check the exit status of the previous command
 # If the package is not installed, the exit status will be non-zero
 if [ $? -ne 0 ]; then
-    echo "Package $QRENCODE is not installed. Installing now."
+    echo "Package $PACKAGE_NAME is not installed. Installing now."
     sudo apt-get update
     sudo apt-get install -y $QRENCODE
 else
-    echo "Package $QRENCODE is already installed."
+    echo "Package $PACKAGE_NAME is already installed."
 fi
-
-# currently for fruitmerge only
-REQUIRED_PACKAGES=("unclutter" "xdotool")
-
-for pkg in "${REQUIRED_PACKAGES[@]}"; do
-    if ! dpkg -l | grep -qw $pkg; then
-        echo "Package $pkg is not installed. Installing now."
-        sudo apt-get update && sudo apt-get install -y $pkg
-    else
-        echo "Package $pkg is already installed."
-    fi
-done
-if ! command -v python3 &> /dev/null
-then
-    echo "Python 3 could not be found. Please install Python 3."
-    exit 1
-fi
-
-# Check for pip
-if ! command -v pip3 &> /dev/null
-then
-    echo "pip3 could not be found. Attempting to install..."
-    sudo apt-get update
-    sudo apt-get install -y python3-pip
-fi
-
-# Install Python packages
-pip3 install websockets pyautogui Pillow
-
-# Attempt to install tkinter for Debian/Ubuntu
-if [ -f /etc/debian_version ]; then
-    sudo apt-get install -y python3-tk
-fi
-
-# end of fruit merge dependencies installation
-
